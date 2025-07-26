9ad73dbac21b43a65bec383d5e2785c7d93dd50b
acgsilva <acgdas.dev@gmail.com>
2025-07-26 20:47:27 -0300

    feat disable e enable add lista altera css para desativar e ativar botao adiciona titulos e label para melhora acessibilidade modifica arquivos js separando as responsabilidades

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,6 @@
 import { darkMode } from "./theme/theme.js";
+import { initTaskEvents } from './tasks/tasks.js';
 $(document).ready(function () {
   darkMode();
-  // Adicionar nova tarefa
-  $('#add-btn').click(function () {
-    const task = $('#list_input').val().trim();
-    if (task) {
-      $('#merc-list').append(`
-        <li>
-          <span>${task}</span>
-          <button class="remove-btn">×</button>
-        </li>
-      `);
-      $('#list_input').val('');
-    }
-  });
-
-  // Adicionar com Enter
-  $('#list_input').keypress(function (e) {
-    if (e.which === 13) {
-      $('#add-btn').click();
-    }
-  });
-
-  // Marcar como concluída
-  $('#merc-list').on('click', 'span', function () {
-    $(this).parent().toggleClass('done');
-  });
-
-  // Remover tarefa
-  $('#merc-list').on('click', '.remove-btn', function () {
-    $(this).parent().fadeOut(200, function () {
-      $(this).remove();
-    });
-  });
+  const taskHandlers = initTaskEvents();
 });
